61fc109beed65e92a1485e7bc1472a4e13cafdb4
IvoryLu <Ivory.Lu@australianwildlife.org>
2019-08-27 16:31:56 +0800

    Basic staff understanding

--- begintf.py
+++ begintf.py
@@ -1,3 +1,13 @@
+
 config = tf.ConfigProto()
+#Allowing GPU memory growth
 config.gpu_options.allow_growth = True
+#To find out which devices your operations and tensors are assigned to, create the session
+#with log_device_placement configuration option set to True. 
+config.log_device_placement = True
 session = tf.Session(config=config)
+
+#Placeholders are used to feed external data into a TensorFlow graph. It allows a value to 
+#be assigned later i.e. a place in the memory where we'll store a value later on. 
+#%%
+#Variables are used to store the state of a graph. Varaibles need a value to be initialized 
