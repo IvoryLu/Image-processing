a15c5041089e26346988939787309fb53cd1c898
Juan Lu (Ivory) <ivorylu.821@gmail.com>
2019-09-23 16:52:57 +0800

    Update begintf.py
    Add input data

--- begintf.py
+++ begintf.py
@@ -1,3 +1,15 @@
+import tensorflow as tf
+import os
+
+old_v = tf.logging.get_verbosity()  #Return how much logging output will be produced. 
+tf.logging.set_verbosity(tf.logging.ERROR)
+
+from tensorflow.examples.tutorial.mnist import input_data
+mnist = input_data.read_data_sets('MNIST_data', one_hot = True) 
+tf.logging.set_verbosity(old_v)
+#one-hot = True argument only means that, in contract to Binary representation,
+#the labels will be presented in a way that only one bie will be on for
+#a specifit digit. 
 
 config = tf.ConfigProto()
 #Allowing GPU memory growth
@@ -11,3 +23,4 @@ session = tf.Session(config=config)
 #be assigned later i.e. a place in the memory where we'll store a value later on. 
 #%%
 #Variables are used to store the state of a graph. Varaibles need a value to be initialized 
+
